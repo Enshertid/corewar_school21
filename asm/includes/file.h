26f70aeb76c8a4e0b7a3e1d455d4ac0f3818ecab
sleng17 <sammert17@yandex.ru>
2020-04-22 11:37:27 +0300

    gitignore ignored files with mask file*

---
+++ asm/includes/file.h
@@ -0,0 +1,24 @@
+#ifndef FILE_H
+# define FILE_H
+
+#include "token.h"
+
+enum	e_file_status
+{
+	FILE_OK,
+	FILE_EMPTY,
+	FILE_NO_FINAL_NEWLINE
+};
+
+typedef enum e_file_status	t_file_status;
+
+typedef struct		s_file
+{
+	const char 		*name;
+	int				fd;
+	t_vector_char	*lines;
+	t_vector_token	*tokens;
+	t_file_status	status;
+}					t_file;
+
+#endif
