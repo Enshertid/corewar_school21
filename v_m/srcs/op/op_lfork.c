10749e83d1a72f0b0f1890a24c8a84eebcdd9a23
Ediego <apicik@ya.ru>
2020-05-10 16:25:44 +0400

    op_fork and op_lfork edit

--- v_m/srcs/op/op_lfork.c
+++ v_m/srcs/op/op_lfork.c
@@ -6,7 +6,7 @@
 /*   By: ediego  <ediego@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 18:46:32 by ediego            #+#    #+#             */
-/*   Updated: 2020/05/09 00:22:46 by ediego           ###   ########.fr       */
+/*   Updated: 2020/05/09 15:41:37 by ediego           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void 		op_lfork(t_vm *vm, t_car *car)
 	t_car	*new;
 	int32_t pos;
 
-	pos = get_2byte(vm, car->position + 2);
-	new = copy_caret(car, (car->position + pos) % MEM_SIZE, ++vm->id_cars);
+	pos = car->position + get_2byte(vm, car->position + 2);
+	new = copy_caret(car, pos % MEM_SIZE, ++vm->id_cars);
 	add_caret(&vm->carriages, new);
 	car->position = (car->position + 3) % MEM_SIZE;
 }
