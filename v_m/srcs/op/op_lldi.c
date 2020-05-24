cd6d7ca87e585e45be688b99cb77cc0bccf14e36
Ediego <apicik@ya.ru>
2020-05-24 14:21:16 +0400

    edit header

--- v_m/srcs/op/op_lldi.c
+++ v_m/srcs/op/op_lldi.c
@@ -6,7 +6,7 @@
 /*   By: ediego  <ediego@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 18:44:25 by ediego            #+#    #+#             */
-/*   Updated: 2020/05/21 00:19:56 by ediego           ###   ########.fr       */
+/*   Updated: 2020/05/23 23:12:30 by ediego           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ void 		op_lldi(t_vm *vm, t_car *car, t_arg *arg)
 	int32_t reg3;
 	
 	(void)arg->first;
-	// if (vm->debug)
-	// 	printf("LLDI(%d): Cycle = %d  POS = %d ", car->id, vm->current_cycle, car->position);
 	car->step = 2;
 	args = vm->arena[(car->position + 1) % MEM_SIZE];
 	if (valid_args(vm, car, args))
@@ -31,12 +29,8 @@ void 		op_lldi(t_vm *vm, t_car *car, t_arg *arg)
 		pos = get_pos(car, sum);
 		value = read_four_bytes(vm, pos);
 		reg3 = vm->arena[(car->position + car->step) % MEM_SIZE];
-		// if (vm->debug)
-		// 	printf(" SUM = %d POS = %d VALUE = %d ", sum, pos, value);
 		set_reg(car, reg3, value, 1);
 	}
 	car->position = get_pos(car, get_arg_step(args, 3, DIR_SIZE / 2));
 	car->step = OP_BYTE;
-	// if (vm->debug)
-	// 	printf(" END pos = %d\n", car->position);
 }
--- v_m/srcs/op/op_zjmp.c
+++ v_m/srcs/op/op_zjmp.c
@@ -6,7 +6,7 @@
 /*   By: ediego  <ediego@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 18:39:34 by ediego            #+#    #+#             */
-/*   Updated: 2020/05/21 00:24:58 by ediego           ###   ########.fr       */
+/*   Updated: 2020/05/23 23:12:45 by ediego           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,11 @@ void 		op_zjmp(t_vm *vm, t_car *car, t_arg *arg)
 	int dir;
 	
 	(void)arg->first;
-	// if (vm->debug)
-	// 	printf("ZJMP(%d): Cycle = %d Pos = %d", car->id, vm->current_cycle, car->position);
 	if (car->carry)
 	{
 		dir = read_two_bytes(vm, (car->position + 1) % MEM_SIZE);
 		car->position = get_pos(car, get_idx(dir));
-		// if (vm->debug)
-		// 	printf(" DIR = %d jump to %d \n", dir, car->position);
 	}
 	else
-	{
 		car->position = (car->position + 3) % MEM_SIZE;
-		// if (vm->debug)
-		// 	printf(" carry = %d End position %d\n", car->carry, car->position);
-	}
 }
