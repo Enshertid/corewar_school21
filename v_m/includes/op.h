3af85a71eab304900001b3f851ac1a9c0adbcfa1
enshertid <enshertid@icloud.com>
2020-05-26 13:02:22 +0300

    aaaa

--- v_m/includes/op.h
+++ v_m/includes/op.h
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #ifndef OP_H
-
 # define OP_H
 
 # define IND_SIZE				2
@@ -25,15 +24,16 @@
 # define MAX_ARGS_NUMBER			4
 # define MAX_PLAYERS				4
 # define MEM_SIZE				(4*1024)
-# define IDX_MOD					(MEM_SIZE / 8)
+# define IDX_MOD				(MEM_SIZE / 8)
 # define CHAMP_MAX_SIZE			(MEM_SIZE / 6)
 
 # define COMMENT_CHAR			'#'
 # define LABEL_CHAR				':'
-# define DIRECT_CHAR				'%'
+# define DIRECT_CHAR			'%'
 # define SEPARATOR_CHAR			','
 
-# define NAME_CMD_STRING			".name"
+#define LABEL_CHARS				"abcdefghijklmnopqrstuvwxyz_0123456789"
+# define NAME_CMD_STRING		".name"
 # define COMMENT_CMD_STRING		".comment"
 
 # define REG_NUMBER				16
